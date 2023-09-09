83d9ea6c6354cba9324263c085f92ba5e4dc4776
Anibal_Kaune <127349390+anibal-kaune@users.noreply.github.com>
2023-09-09 10:13:22 -0300

    login y registro agregado, arreglar logo

--- src/app/registro/registro.page.ts
+++ src/app/registro/registro.page.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { AlertController, IonicModule, NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-registro',
@@ -7,9 +9,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegistroPage implements OnInit {
 
-  constructor() { }
+  formularioRegistro: FormGroup;
+
+  constructor(public fb: FormBuilder,
+    public alertController: AlertController,
+    public navCtrl: NavController) { 
+      this.formularioRegistro = this.fb.group({
+        'nombre': new FormControl("", Validators.required),
+        'rut': new FormControl("", Validators.required),
+        'correo': new FormControl("", Validators.required),
+        'password': new FormControl("", Validators.required),
+        'confirmacionPassword': new FormControl("", Validators.required)
+        }); 
+    }
 
   ngOnInit() {
   }
+
+  async guardar(){
+    var f = this.formularioRegistro.value;
+
+    if(this.formularioRegistro.invalid){
+      const alert = await this.alertController.create({
+        header: 'Campos incompletos',
+        message: 'Debes llenar todos los campos.',
+        buttons: ['Aceptar']
+      });
+
+      await alert.present();
+      return;
+    }
+    var usuario = {
+      nombre: f.nombre,
+      rut: f.rut,
+      correo: f.correo,
+      password: f.password
+    }
+
+    localStorage.setItem('usuario',JSON.stringify(usuario));
+    window.location.href='/login';
+  }
+
 
 }
