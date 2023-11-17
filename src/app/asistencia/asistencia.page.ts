b976b4155bf1a7e8ba36b1eea6e2f9a6ddbae83b
Anibal_Kaune <ani.kaune@duocuc.cl>
2023-11-16 22:33:22 -0300

    Alerta localización

--- src/app/asistencia/asistencia.page.ts
+++ src/app/asistencia/asistencia.page.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { StorageService } from '../services/storage.service';
 import { Usuario } from '../models/usuario';
+import { Geolocation } from '@capacitor/geolocation';
+import { AlertController } from '@ionic/angular';
 
 @Component({
   selector: 'app-asistencia',
@@ -12,7 +14,8 @@ export class AsistenciaPage implements OnInit {
   dataProfe:string | null;
   userAlum: string | null;
 
-  constructor(public storageService: StorageService) {
+  constructor(public storageService: StorageService,
+              public alertController: AlertController) {
     this.dataProfe = localStorage.getItem("dataProfeCamera");
     
     let value = JSON.parse(localStorage.getItem("usuario")!);
@@ -29,5 +32,19 @@ export class AsistenciaPage implements OnInit {
   }
   ngOnInit() {
   }
+
+  async printCurrentPosition() {
+    const coordinates = await Geolocation.getCurrentPosition();
+
+    const latitud: number | any= coordinates.coords.latitude;
+    const longitud= coordinates.coords.longitude;
+
+    const alert = await this.alertController.create({
+      header: 'Localización actual',
+      message: 'Latitud: ' + latitud + ' ' + 'Longitud: ' + longitud,
+      buttons: ['Aceptar']
+    });
+
+  };
 
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -49,10 +49,4 @@ export class HomePage {
       }
     }
 
-  async printCurrentPosition() {
-    const coordinates = await Geolocation.getCurrentPosition();
-
-    console.log('Current position:', coordinates);
-  };
-
 }
